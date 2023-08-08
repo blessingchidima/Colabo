0bfbe36c5dec20837ec7b95b3e807beef88a8feb
unknown <fashkenny>
2023-08-08 16:28:15 +0100

    pushing from kenny

--- fe--/src/delaration.d.ts
+++ fe--/src/delaration.d.ts
@@ -1,3 +1,6 @@
 declare module "*hookform/resolvers/yup"
 declare module "*react-hook-form"
 declare module "*yup"
+declare module '*react-input-emoji'
+declare module '*@formkitAutoanimate/react'
+declare module '@react-icons'
